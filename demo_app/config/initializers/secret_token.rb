0cc491669f37ac26c3c65f0155c49c0e96530021
efoster0512 <eric_foster73@hotmail.com>
2012-03-06 22:36:57 -0500

    first commit

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b907381e5f4d0f34b722849602f8933096ceb95de0313a0d72d3c37fa55cc17459f762e63b0da174c9515640a8406a2481c30ef5718f905582e12d72492c985b'
---
+++ fixDateFormatError.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo sed -i 's/ 00:00:00.000000000Z//' /var/lib/gems/1.8/specifications/*
